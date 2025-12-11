30c8e11291882927d575d2524858a5904f068e17
Rex <rexyyong@gmail.com>
2025-12-11 14:13:27 +0800

    feat: Frontend for buswhere

--- frontend/app_name/src/App.js
+++ frontend/app_name/src/App.js
@@ -1,25 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+
+
+
+
+function HeaderOne() {
+  return (
+    <h1 className='HeadOne'>Bus Where?!</h1>
+  );
+}
+
+function AddBusStopCode() {
+  return (
+    <Box>
+      <Grid container spacing={1} className='AddBusStopCodeCss'>
+        <TextField required id="outlined" label="Enter Bus stop Code" variant="outlined" size='small' />
+        <Button variant="contained" size='small'>Add</Button>
+      </Grid>
+    </Box>
+
+  );
+}
+
+function AddedBusStopCode() {
+  return (
+    <Box className='AddedBusStopBox'>
+      <Typography variant="h6">Added Bus Stops</Typography>
+      <Grid container spacing={3} className='AddedBusStopCodeCss'>
+        <Typography variant="body1">Added Bus stop</Typography>
+        <Button variant="outlined" size='small' startIcon={<DeleteIcon />}>Remove</Button>
+      </Grid>
+    </Box>
+  );
+}
+
+function PreviewBusTimings() {
+  return (
+    <Box className='PreviewBusTimingsBox'>
+      <Typography variant="h6">Bus Timings Preview</Typography>
+      <Box className='PreviewBusTimingsImage'>
+        {/* Image will be displayed here */}
+        <Typography variant="h6">image will be displayed here</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+function ConvertedBinaryImage() {
+  return (
+    <Box className='PreviewBusTimingsBox'>
+      <Typography variant="h6">Converted Preview</Typography>
+      <Box className='PreviewBusTimingsImage'> 
+        {/* Image will be displayed here */}
+        <Typography variant="h6">image will be displayed here</Typography>
+      </Box>
+    </Box>
+  ); 
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderOne />
+      <AddBusStopCode />
+      <AddedBusStopCode />
+      <PreviewBusTimings />
+      <ConvertedBinaryImage />
     </div>
-  );
+   );
 }
 
 export default App;
